fbf29f5564b44053948a9feab7cb8f32944055d4
yang bo <yangbo@yangdeiMac.local>
2017-06-28 16:55:33 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Demo02 from './demo02';
+import Demo05 from './demo05';
 
 const root = document.querySelector('#root');
 
-render(<Demo02 />, root);
+render(<Demo05 />, root);
